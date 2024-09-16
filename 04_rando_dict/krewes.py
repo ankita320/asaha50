bce7cfeea7ec0f9dffdc12cfb9376b7b7e32d642
Ankita Saha <76600603+ankita320@users.noreply.github.com>
2024-09-16 18:00:12 -0400

    Create krewes.py

---
+++ 04_rando_dict/krewes.py
@@ -0,0 +1,26 @@
+import random
+krewes = {
+           4: [ 
+		'DUA','TAWAB','EVA','JACK','VICTOR','EVAN','JASON','COLYI','IVAN','TANZEEM',
+		'TAHMIM','STANLEY','LEON','NAOMI','NIA','ANASTASIA','JADY','BRIAN','JACOB',
+		'ALEX','CHONGTIAN','DANNY','MARCO','ABIDUR','ANKITA','ANDY','ETHAN','AMANDA',
+		'AIDAN','LINDA','QIANJUN','JIAYING','KISHI'
+		],
+           5: [ 
+                'ADITYA','MARGIE','RACHEL','ALEXANDER','ZIYAD','DANNY','ENDRIT','CADEN',
+                'VEDANT','SUHANA','KYLE','KEVIN','RAYMOND','CHRISTOPHER','JONATHAN','SASHA',
+                'NAFIYU','TIM','WILL','DANIEL','BENJAMIN','CLAIRE','CHLOE','STELLA','TRACY',
+                'JESSICA','JACKIE','WEN YUAN','YINWEI','TIFFANY','JAYDEN DANIEL','PRINCEDEN' 
+              ]
+         }
+
+num = random.randint(0,1)
+
+if num == 0:
+  d = krewes.get(4)
+elif num == 1:
+  d = krewes.get(5)
+  
+n = random.randint(0, len(d)-1)
+fin = d[n]
+return fin
