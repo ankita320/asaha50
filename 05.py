e5177344202c461ea1231ac2c9c4faadc967b4d0
Ankita Saha <76600603+ankita320@users.noreply.github.com>
2024-09-17 18:57:02 -0400

    Delete 05.py

--- 05.py
+++
@@ -1,9 +0,0 @@
-def bitstream(){
-    '''
-list=[dictionary_4, dictionary_5]
-dictionary_4={name:ducky,]
-splice first at $$$ and put intwo tuple of length 3
-splice at @@@, put tuples in list
-
-'''
-    }
