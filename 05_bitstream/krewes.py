a249a0c99cf425d4d172e60357280799d0894772
Ankita Saha <76600603+ankita320@users.noreply.github.com>
2024-09-17 18:57:30 -0400

    Create krewes.py

---
+++ 05_bitstream/krewes.py
@@ -0,0 +1,9 @@
+def bitstream(){
+    '''
+list=[dictionary_4, dictionary_5]
+dictionary_4={name:ducky,]
+splice first at $$$ and put intwo tuple of length 3
+splice at @@@, put tuples in list
+
+'''
+    }
