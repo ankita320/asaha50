49245f4b458a3ba0dfc7e7beffc1dd1cacebe4d5
Ankita Saha <76600603+ankita320@users.noreply.github.com>
2024-09-18 17:32:43 -0400

    Update krewes.py
    random person

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -14,8 +14,15 @@ def bitstream(){
             d[4].append({k[1]: k[2]})
         elif k[0] == 5:
             d[5].append({k[1]: k[2]})
-    
-    
+            
+    num = random.randint(4,5)
+    p = random.randint(len(d.get(num))-1)
+
+    k = list(d.get(num)[p])[0]
+    v = list(d.get(num)[p])[1]
+
+    return "PERIOD: " + num + "; NAME: " + k + "; VALUE: " + v
+        
 
     '''
 list=[dictionary_4, dictionary_5]
