970e10f894faf769bdc0b21b1fc0e7a0e094c5d5
Ankita Saha <76600603+ankita320@users.noreply.github.com>
2024-09-20 00:00:06 -0400

    Update occup.py

--- occup.py
+++ occup.py
@@ -1,21 +1,20 @@
 import random
 import csv
-def occup_d():
+
+def job_d():
     with open("./occupations.csv", mode ='r') as file:
-       d = {}
-       csvFile = csv.DictReader(file)
-       for lines in csvFile:
+        d = {}
+        csvFile = csv.DictReader(file)
+        for lines in csvFile:
             d[lines.get('Job Class')] = float(lines.get('Percentage'))
-       return d
-                
-def rando_occup(x):
-    sum_v = 0
-    r = random.uniform(0,x.get('Total'))
-    for i in x:
-        print(i)
-        print(x.get(i))
-        sum_v += x.get(i)
-        print(sum)
-        if sum_v > r:
-            return {i:x.get(i)}
-            
+        
+        sum_v = 0
+        r = random.uniform(0,d.get('Total'))
+        for i in d:
+        #print(i)
+        #print(d.get(i))
+            sum_v += d.get(i)
+        #print(sum)
+            if sum_v > r:
+                return {i:d.get(i)}
+print(job_d())
