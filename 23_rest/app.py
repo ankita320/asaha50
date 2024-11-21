bdf4deee6b50bde8b44a6255dd45eaea2ab3adff
Ankita Saha <76600603+ankita320@users.noreply.github.com>
2024-11-20 21:21:55 -0500

    finished k23

--- 23_rest/app.py
+++ 23_rest/app.py
@@ -1,26 +1,23 @@
 from flask import Flask, render_template
+import json
+import urllib.request
+
 app = Flask(__name__)
 
 
 @app.route("/")
 
-f = open("../key_nasa.txt", "r")
-key = f.read()
 
-def hello_world():
-    return "No hablo queso!"
-
-
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Q1: Can all of your teammates confidently predict the URL to use to load this page?
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-@app.route("/my_foist_template") 
-def test_tmplt():
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Q2: What is the significance of each argument? Simplest, most concise answer best.
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    return render_template( 'model_tmplt.html')
+def api():
+   file = open("key_nasa.txt")
+   key = file.read().strip()
+   url = urllib.request.urlopen(f"https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key={key}")
+   json_d = url.read()
+   info = json.loads(json_d.strip())
+   
+   img = info["photos"][0]["img_src"]
+   txt = info["photos"][0]["earth_date"]
+   return render_template("main.html", img=img, txt=txt)
 
 
 if __name__ == "__main__":
